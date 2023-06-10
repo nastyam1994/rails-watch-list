c7b23c8292fcce6374fa0958e3f0c1c4fa70f56c
Nastya Mikhailova <nastyam_94@bk.ru>
2023-06-10 16:54:07 +0800

    Styling

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-// import TypewriterController from "./typewriter_controller"
-// application.register("typewriter", TypewriterController)
+import TomSelectController from "./tom_select_controller"
+application.register("tom-select", TomSelectController)
+
+import TypewriterController from "./typewriter_controller"
+application.register("typewriter", TypewriterController)
