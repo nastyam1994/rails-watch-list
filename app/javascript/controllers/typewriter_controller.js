58c425a66563205b0439dd2a52755860adafe260
Nastya Mikhailova <nastyam_94@bk.ru>
2023-06-10 14:43:32 +0800

    Commented out TypeWriter class

--- app/javascript/controllers/typewriter_controller.js
+++ app/javascript/controllers/typewriter_controller.js
@@ -10,43 +10,43 @@ export default class extends Controller {
   }
 }
 
-class TypeWriter {
-  constructor(txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.isDeleting = false;
-  }
+// class TypeWriter {
+//   constructor(txtElement, words, wait = 3000) {
+//     this.txtElement = txtElement;
+//     this.words = words;
+//     this.txt = '';
+//     this.wordIndex = 0;
+//     this.wait = parseInt(wait, 10);
+//     this.isDeleting = false;
+//   }
 
-  type() {
-    const current = this.wordIndex % this.words.length;
-    const fullTxt = this.words[current];
+//   type() {
+//     const current = this.wordIndex % this.words.length;
+//     const fullTxt = this.words[current];
 
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+//     if (this.isDeleting) {
+//       this.txt = fullTxt.substring(0, this.txt.length - 1);
+//     } else {
+//       this.txt = fullTxt.substring(0, this.txt.length + 1);
+//     }
 
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+//     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-    let typeSpeed = 300;
+//     let typeSpeed = 300;
 
-    if (this.isDeleting) {
-      typeSpeed /= 2;
-    }
+//     if (this.isDeleting) {
+//       typeSpeed /= 2;
+//     }
 
-    if (!this.isDeleting && this.txt === fullTxt) {
-      typeSpeed = this.wait;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.wordIndex++;
-      typeSpeed = 500;
-    }
+//     if (!this.isDeleting && this.txt === fullTxt) {
+//       typeSpeed = this.wait;
+//       this.isDeleting = true;
+//     } else if (this.isDeleting && this.txt === '') {
+//       this.isDeleting = false;
+//       this.wordIndex++;
+//       typeSpeed = 500;
+//     }
 
-    setTimeout(() => this.type(), typeSpeed);
-  }
-}
+//     setTimeout(() => this.type(), typeSpeed);
+//   }
+// }
